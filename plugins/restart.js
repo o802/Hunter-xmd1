51f68a874f21456d5aed201b2979a05ec6040f36
Obed TechX <obedomoke360@gmail.com>
2025-05-16 19:51:47 +0300

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart KHAN-MD",  
+    desc: "Restart HUNTER-XMD",  
     category: "owner",  
     filename: __filename  
 },  
